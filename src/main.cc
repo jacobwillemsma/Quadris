90fff9c28c2663da0ad7f75619b890644822f5fe
jacobwills <jacob.willemsma@gmail.com>
2014-04-02 00:07:12 -0400

    Added main.cc

---
+++ src/main.cc
@@ -0,0 +1,77 @@
+#include "board.h"
+#include "iblock.h"
+#include "jblock.h"
+#include "lblock.h"
+#include "tblock.h"
+#include "oblock.h"
+#include "zblock.h"
+#include "sblock.h"
+#include "interpreter.h"
+#include "score.h"
+#include "block.h"
+#include "nextblock.h"
+#include "display.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+	bool textOnly = false;
+	string filename = "sequence.txt";
+	int startlevel = 0;
+
+	// Start by going through the options.
+	for (int i = 0; i < argc; i++)
+    {
+         string arg = argv[i];
+         if (arg == "-text")
+         	textOnly = true;
+         if (ar == "-scriptfile")
+         	filename = argv[i + 1];
+         if (ar == "-startlevel")
+         {
+            int num;
+            stringstream ss(argv[i + 1]);
+            ss >> num;
+            startlevel = num;
+         }
+         /*if (ar == "-seed")
+         {
+            int num;
+            stringstream ss(argv[i + 1]);
+            ss >> num;
+            srand(num);
+         }*/
+    }
+
+    Interpreter *i = new Interpreter;
+    if (textOnly)
+    	Board *b = new Board(...) // Initiated without the xWindow chunk.
+    else
+		Board *b = new Board(...); // initiated with xWindow.
+	string input;
+	
+	// Deal with the commands issued to the game.
+	// Need to somehow implement current block and next block seemlessly.  Because next block needs to be known here.
+	while (cin >> command) {
+		if (input[0] >= 48 && input[0] <= 57) {
+			// We have a number before our command.
+			istringstream ss;
+			int i = 0;
+			int number;
+			string command;
+			while (input[i] >= 48 && input[i] <= 57) {
+				ss << input[i];
+				i++;
+			}
+			ss >> number;
+			command = input.substr(i);
+			i.multipleCommand(number, command, b);
+		}
+		else {
+			i.processCommand(input, b);
+		}
+		// Print out the board every time.
+		cout << b;
+	}
+}
